dda0f1c8a9f1d475713b3f2c2682413ea8154c65
aleprado89 <aleprado89@gmail.com>
2023-09-04 18:29:05 -0300

    1

---
+++ TP4/TP4/trabajo1.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="trabajo1.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Label ID="Label1" runat="server" Text="Suscripción a Noticias:"></asp:Label>
+        <p>
+            <asp:Label ID="Label2" runat="server" Text="Direccion de Correo para la Suscripción: "></asp:Label>
+        </p>
+            <asp:TextBox ID="correoTB" runat="server" Width="286px"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="Label3" runat="server" Text="Seleccione los temas a los que se quiera suscribir: "></asp:Label>
+        <p>
+            <asp:CheckBox ID="politicaCB" runat="server" Text="Política" />
+        </p>
+        <p>
+            <asp:CheckBox ID="deportesCB" runat="server" Text="Deportes" />
+        </p>
+        <p>
+            <asp:CheckBox ID="espectaculoCB" runat="server" Text="Espectáculo" />
+        </p>
+        <p>
+            <asp:CheckBox ID="internacionalCB" runat="server" Text="Internacional" />
+        </p>
+        <p>
+            <asp:CheckBox ID="sociedadCB" runat="server" Text="Sociedad" />
+        </p>
+        <p>
+            <asp:Button ID="confirmarBT" runat="server" OnClick="verificarPersonaBT_Click" Text="Confirmar Selección" Width="140px" />
+        </p>
+        <p>
+            <asp:Label ID="resultadoLB" runat="server" Text="-"></asp:Label>
+        </p>
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ TP4/TP4/trabajo1.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+
+    //VARIABLES
+
+    string correo, seleccion;
+
+    //METODOS
+
+    //Load
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    //EVENTOS
+
+    //Verificar Persona
+    protected void verificarPersonaBT_Click(object sender, EventArgs e)
+    {
+        correo = correoTB.Text;
+
+        if (correo != "")
+        {
+            int cantidadSeleccionada = 0;
+            seleccion = "Temas Seleccionados (";
+
+            if (politicaCB.Checked == true)
+            {
+                seleccion = seleccion + "-" + "Política";
+                cantidadSeleccionada++;
+            }
+            if (deportesCB.Checked == true)
+            {
+                seleccion = seleccion + "-" + "Deportes";
+                cantidadSeleccionada++;
+            }
+            if (espectaculoCB.Checked == true)
+            {
+                seleccion = seleccion + "-" + "Espectáculo";
+                cantidadSeleccionada++;
+            }
+            if (internacionalCB.Checked == true)
+            {
+                seleccion = seleccion + "-" + "Internacional";
+                cantidadSeleccionada++;
+            }
+            if (sociedadCB.Checked == true)
+            {
+                seleccion = seleccion + "-" + "Sociedad";
+                cantidadSeleccionada++;
+            }
+            seleccion = seleccion + "-).";            
+
+            if (cantidadSeleccionada == 0)
+            {
+                resultadoLB.Text = "Falta seleccionar los temas de interés.";
+            }
+            else
+            {
+                resultadoLB.Text = correo + " - " + seleccion;
+            }
+        }
+        else
+        {
+            resultadoLB.Text = "Falta registrar una direccion de correo válida.";
+        }
+    }
+}
